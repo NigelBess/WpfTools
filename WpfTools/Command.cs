53fff7f3bc52a16d075e20b5af8ee87d6618c438
NigelBess <nigelbess@gmail.com>
2021-06-06 13:05:11 -0700

    first commit

---
+++ WpfTools/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfTools
+{
+    public class Command:ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+        public Command(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            canExecute ??= O => true;
+            Initialize(execute, canExecute);
+        }
+        public Command(Action execute, Func<bool> canExecute = null)
+        {
+            canExecute ??= () => true;
+
+            Initialize(o => execute(), o => canExecute());
+        }
+        private void Initialize(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ WpfTools/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WpfTools
+{
+    public static class Extensions
+    {
+        public static void Bind(this Notifier viewModel, string propertyName, Action changeAction)
+        {
+            viewModel.PropertyChanged += (p, e) =>
+            {
+                if (e.PropertyName == propertyName) changeAction();
+            };
+        }
+        public static void Bind<T>(this T viewModel, string propertyName, Action<T> changeAction) where T:Notifier
+        {
+            viewModel.PropertyChanged += (p, e) =>
+            {
+                if (e.PropertyName == propertyName) changeAction(viewModel);
+            };
+        }
+    }
+}
